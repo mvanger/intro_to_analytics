eb414f33cc656680cf39384abf5d8f6ae58578c4
mvanger <mvanger@nyu.edu>
2013-10-16 20:55:58 -0500

    Searches through all resumes in a folder

--- Resumes/resume.rb
+++ Resumes/resume.rb
@@ -3,58 +3,73 @@ require "pdf-reader"
 require "pry"
 require "stopwords"
 
-stopwords = Stopwords::STOP_WORDS
+# This is an array of stop words
+# The @ symbol means it is an instance variable, rather than a local variable
+# So methods have access to it
+@stopwords = Stopwords::STOP_WORDS
 
-# This loads a PDF as a Ruby object
-reader = PDF::Reader.new(PATH NAME HERE)
-
-# Some resumes may be more than one page
-# So just loop through the reader.pages array
-text = ""
-
-# This sets text as a string of all the text in a resume
-reader.pages.each do |pp|
-  text = text + " "
-  text = text + pp.text
-end
-
-# Replaces , ( ) with empty strings
-# What are some other symbols?
-# Also stop words
-text.gsub!(",","")
-text.gsub!("(","")
-text.gsub!(")","")
-# This ● is throwing an error
-# But it works in pry for some reason
-# text.gsub!("●","")
-
-# Turns string into an array
-# It splits on the spaces (" ")
-arr = text.split
+# This stores the path of all the resumes in our year
+resumes = Dir["Analytics/Intro to Analytics/MSiA 400 Resumes/Resume Packet NU MSiA 2013-2014/*"]
 
 # Instantiates an empty hash
 # A hash is a key: value pair
 # So it will store the keywords and their counts
-keywords = {}
+@keywords = {}
 
-# Loops through array
-# If keyword is present, adds 1 to the counter
-# If not present, instantiates it with a count of 1
-arr.each do |word|
-  if stopwords.include?("#{word}") == false
-    if keywords.has_key?(:"#{word}")
-      keywords[:"#{word}"] = keywords[:"#{word}"] + 1
-    else
-      keywords[:"#{word}"] = 1
+# This is a method that looks through a resume and updates the @keywords hash
+def keyword_search(path_name)
+  # This loads a PDF as a Ruby object
+  reader = PDF::Reader.new(path_name)
+
+  # Some resumes may be more than one page
+  # So just loop through the reader.pages array
+  text = ""
+
+  # This sets text as a string of all the text in a resume
+  reader.pages.each do |pp|
+    text = text + " "
+    text = text + pp.text
+  end
+
+  # Replaces , ( ) with empty strings
+  # What are some other symbols?
+  # &, -
+  ## Also stop words
+  text.gsub!(",","")
+  text.gsub!("(","")
+  text.gsub!(")","")
+  # This ● is throwing an error
+  # But it works in pry for some reason
+  # text.gsub!("●","")
+
+  # Turns string into an array
+  # It splits on the spaces (" ")
+  arr = text.split
+
+  # Loops through array
+  # If keyword is present, adds 1 to the counter
+  # If not present, instantiates it with a count of 1
+  # binding.pry
+  arr.each do |word|
+    if @stopwords.include?("#{word}") == false
+      if @keywords.has_key?(:"#{word}")
+        @keywords[:"#{word}"] = @keywords[:"#{word}"] + 1
+      else
+        @keywords[:"#{word}"] = 1
+      end
     end
   end
 end
 
-# metrics.sort_by {|_key, value| value}
+# This loops through all the resumes and calls the keyword_search method
+resumes.each do |r|
+  keyword_search(r)
+end
 
-keywords = keywords.sort_by {|key, value| value}
+# This sorts the keywords by value
+@keywords = @keywords.sort_by {|key, value| value}
 # Prints keywords and their count to the screen
-keywords.each do |key, value|
+@keywords.each do |key, value|
   puts "#{key}: #{value}"
 end
 
@@ -62,6 +77,7 @@ end
 ### I had an error, but I think it's fixed
 
 # It may be a good idea to downcase everything first?
+# There are a lot of extra characters in the results
 
 # This is stuff I used for testing
 # binding.pry
