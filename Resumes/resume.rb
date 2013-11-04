45ff6e42e37f6e2ef433193247e977d383a84b25
mvanger <mvanger@nyu.edu>
2013-11-03 21:50:02 -0600

    flagged problem resumes, counted word pairs and individual keywords

--- Resumes/resume.rb
+++ Resumes/resume.rb
@@ -17,7 +17,7 @@ require 'tesseract'
 @stopwords = Stopwords::STOP_WORDS
 
 # This stores the path of all the resumes in the nonanalytics folder
-resumes = Dir["Analytics/Intro to Analytics/MSiA 400 Resumes/Other as PNG/*"]
+resumes = Dir["Analytics/Intro to Analytics/MSiA 400 Resumes/Betas as PNG/*"]
 
 # Instantiates an empty hash
 # A hash is a key: value pair
@@ -42,10 +42,10 @@ def keyword_search(path_name)
   # Sets text as the OCR extracted text
   text = @tesseract.text_for(path_name).strip
 
-  # Replaces , ( ) with empty strings
+  # Replaces , ( ) [ ]with empty strings
   # What are some other symbols?
   # &, -
-  ## Also stop words
+  # Also makes everything lower case
   text.gsub!(",","")
   text.gsub!("(","")
   text.gsub!(")","")
@@ -60,19 +60,27 @@ def keyword_search(path_name)
   # It splits on the spaces (" ")
   arr = text.split
 
+  # This flags any resumes that don't contain 'and' or 'the'
+  # if !(arr.include?("and") || arr.include?("the"))
+  #   puts path_name
+  # end
+
+  # These remove the stopwords from the arr array
+  # @stopwords.each do |s|
+  #   arr.delete(s)
+  # end
+
   # Sets the word array to be all pairs in the arr array
-  # Doesn't check stopwords her
-  ## I can go and redo it if necessary
-  word_array = []
-  for i in 0..arr.length-2
-    word_array << arr[i] + "_" + arr[i+1]
-  end
+  # word_array = []
+  # for i in 0..arr.length-2
+  #   word_array << arr[i] + "_" + arr[i+1]
+  # end
 
   # Loops through array
   # If keyword is present, adds 1 to the counter
   # If not present, instantiates it with a count of 1
   # binding.pry
-  word_array.each do |word|
+  arr.each do |word|
     if @stopwords.include?("#{word}") == false
       if @keywords.has_key?(:"#{word}")
         @keywords[:"#{word}"] = @keywords[:"#{word}"] + 1
@@ -87,32 +95,17 @@ end
 resumes.each do |r|
   keyword_search(r)
 end
-# binding.pry
+
 # This sorts the keywords by value
 @keywords = @keywords.sort_by {|key, value| value}
 
 # Prints keywords and their count to the screen
 # Creates a new .txt file and prints the keys and values to that file
-@f = File.new("other_double_words.txt", "w")
+@f = File.new("betas_keyword_count.txt", "w")
 @keywords.each do |key, value|
   # puts "#{key}: #{value}"
   @f.write("#{key}\t#{value}\n")
 end
 @f.close
 
-
-
-# My resume, somehow, doesn't use any words twice. Or I have an error somewhere.
-### I had an error, but I think it's fixed
-
-# It may be a good idea to downcase everything first?
 # There are a lot of extra characters in the results
-
-# This is stuff I used for testing
-# binding.pry
-# test = keywords.to_a
-# test2 = []
-# test.each do |x|
-#   test2 << x[1]
-# end
-# puts test2.max
